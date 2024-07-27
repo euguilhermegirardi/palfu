37dc3332f0d90481b14dd190722fb789f8ded238
euguilhermegirardi <girardi.gui@icloud.com>
2024-07-27 17:05:21 -0300

    update list

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -6,374 +6,402 @@ export interface IVehicle {
 export const list: IVehicle[] = [
   {
     id: 1,
-    number: 'ams5204',
-  },
-  {
-    id: 2,
     number: 'AMS5204',
   },
   {
-    id: 3,
+    id: 2,
     number: 'FJW8741',
   },
   {
-    id: 4,
+    id: 3,
     number: 'EJI3D21',
   },
   {
-    id: 5,
+    id: 4,
     number: 'FLR3518',
   },
   {
-    id: 6,
+    id: 5,
     number: 'ERJ2749',
   },
   {
-    id: 7,
+    id: 6,
     number: 'EUX1106',
   },
   {
-    id: 8,
+    id: 7,
     number: 'FLV3365',
   },
   {
-    id: 9,
+    id: 8,
     number: 'GCZ8533',
   },
   {
-    id: 10,
+    id: 9,
     number: 'GGV6330',
   },
   {
-    id: 11,
+    id: 10,
     number: 'DSO3C69',
   },
   {
-    id: 12,
+    id: 11,
     number: 'ERP3014',
   },
   {
-    id: 13,
+    id: 12,
     number: 'GCIO250',
   },
   {
-    id: 14,
+    id: 13,
     number: 'EJT9688',
   },
   {
-    id: 15,
+    id: 14,
     number: 'FXQ8B89',
   },
   {
-    id: 16,
+    id: 15,
     number: 'CSF0185',
   },
   {
-    id: 17,
+    id: 16,
     number: 'CSE1710',
   },
   {
-    id: 18,
+    id: 17,
     number: 'DEH2G28',
   },
   {
-    id: 19,
+    id: 18,
     number: 'FPU7879',
   },
   {
-    id: 20,
+    id: 19,
     number: 'GDK1I21',
   },
   {
-    id: 21,
+    id: 20,
     number: 'FMP2264',
   },
   {
-    id: 22,
+    id: 21,
     number: 'BQE2982',
   },
   {
-    id: 23,
+    id: 22,
     number: 'DMM2274',
   },
   {
-    id: 24,
+    id: 23,
     number: 'KCP6G04',
   },
   {
-    id: 25,
+    id: 24,
     number: 'RMH8B70',
   },
   {
-    id: 26,
+    id: 25,
     number: 'ERI4449',
   },
   {
-    id: 27,
+    id: 26,
     number: 'FWC1378',
   },
   {
-    id: 28,
+    id: 27,
     number: 'EGE3622',
   },
   {
-    id: 29,
+    id: 28,
     number: 'EYA4C74',
   },
   {
-    id: 30,
+    id: 29,
     number: 'EIB1I00',
   },
   {
-    id: 31,
+    id: 30,
     number: 'CZC2E05',
   },
   {
-    id: 32,
+    id: 31,
     number: 'FNU9775',
   },
   {
-    id: 33,
+    id: 32,
     number: 'EJT8578',
   },
   {
-    id: 34,
+    id: 33,
     number: 'DGC3373',
   },
   {
-    id: 35,
+    id: 34,
     number: 'IHC5I72',
   },
   {
-    id: 36,
+    id: 35,
     number: 'DQP4A24',
   },
   {
-    id: 37,
+    id: 36,
     number: 'GFU1G17',
   },
   {
-    id: 38,
+    id: 37,
     number: 'ESA7890',
   },
   {
-    id: 39,
+    id: 38,
     number: 'FVT0309',
   },
   {
-    id: 40,
+    id: 39,
     number: 'QPB2637',
   },
   {
-    id: 41,
+    id: 40,
     number: 'FKA4308',
   },
   {
-    id: 42,
+    id: 41,
     number: 'FKH2C80',
   },
   {
-    id: 43,
+    id: 42,
     number: 'FNJ8917',
   },
   {
-    id: 44,
+    id: 43,
     number: 'AYA9C52',
   },
   {
-    id: 45,
+    id: 44,
     number: 'ERJ4379',
   },
   {
-    id: 46,
+    id: 45,
     number: 'FKJ1B09',
   },
   {
-    id: 47,
+    id: 46,
     number: 'HRD1644',
   },
   {
-    id: 48,
+    id: 47,
     number: 'FKW5080',
   },
   {
-    id: 49,
+    id: 48,
     number: 'BLM7616',
   },
   {
-    id: 50,
+    id: 49,
     number: 'EAC0G80',
   },
   {
-    id: 51,
+    id: 50,
     number: 'EIG3560',
   },
   {
-    id: 52,
+    id: 51,
     number: 'BIC2689',
   },
   {
-    id: 53,
+    id: 52,
     number: 'HOB2D91',
   },
   {
-    id: 54,
+    id: 53,
     number: 'CVV0633',
   },
   {
-    id: 55,
+    id: 54,
     number: 'EAQ1626',
   },
   {
-    id: 56,
+    id: 55,
     number: 'FNH0657',
   },
   {
-    id: 57,
+    id: 56,
     number: 'EAQ0B16',
   },
   {
-    id: 58,
+    id: 57,
     number: 'FDJ4J32',
   },
   {
-    id: 59,
+    id: 58,
     number: 'FPD2151',
   },
   {
-    id: 60,
+    id: 59,
     number: 'FKA1060',
   },
   {
-    id: 61,
+    id: 60,
     number: 'EAQ3B48',
   },
   {
-    id: 62,
+    id: 61,
     number: 'LMC2I59',
   },
   {
-    id: 63,
+    id: 62,
     number: 'ENJ2801',
   },
   {
-    id: 64,
+    id: 63,
     number: 'GGF0841',
   },
   {
-    id: 65,
+    id: 64,
     number: 'DEA3628',
   },
   {
-    id: 66,
+    id: 65,
     number: 'CHB4409',
   },
   {
-    id: 67,
+    id: 66,
     number: 'DJD2236',
   },
   {
-    id: 68,
+    id: 67,
     number: 'DXB0345',
   },
   {
-    id: 69,
+    id: 68,
     number: 'FXH1658',
   },
   {
-    id: 70,
+    id: 69,
     number: 'HBQ0285',
   },
   {
-    id: 71,
+    id: 70,
     number: 'PUP3G52',
   },
   {
-    id: 72,
+    id: 71,
     number: 'DAN9648',
   },
   {
-    id: 73,
+    id: 72,
     number: 'CDH2444',
   },
   {
-    id: 74,
+    id: 73,
     number: 'DNL5572',
   },
   {
-    id: 75,
+    id: 74,
     number: 'FYP1D03',
   },
   {
-    id: 76,
+    id: 75,
     number: 'CYO4D50',
   },
   {
-    id: 77,
+    id: 76,
     number: 'ECI1457',
   },
   {
-    id: 78,
+    id: 77,
     number: 'PQL7834',
   },
   {
-    id: 79,
+    id: 78,
     number: 'AGX0414',
   },
   {
-    id: 80,
+    id: 79,
     number: 'FLZ0833',
   },
   {
-    id: 81,
+    id: 80,
     number: 'FLX9232',
   },
   {
-    id: 82,
+    id: 81,
     number: 'HGG9244',
   },
   {
-    id: 83,
+    id: 82,
     number: 'AHJ7709',
   },
   {
-    id: 84,
+    id: 83,
     number: 'FKT9747',
   },
   {
-    id: 85,
+    id: 84,
     number: 'CEM2818',
   },
   {
-    id: 86,
+    id: 85,
     number: 'ONT3F53',
   },
   {
-    id: 87,
+    id: 86,
     number: 'CBH2353',
   },
   {
-    id: 88,
+    id: 87,
     number: 'FQL8C24',
   },
   {
-    id: 89,
+    id: 88,
     number: 'CRA2440',
   },
   {
-    id: 90,
+    id: 89,
     number: 'HSY2H13',
   },
   {
-    id: 91,
+    id: 90,
     number: 'BQZ9D93',
   },
   {
-    id: 92,
+    id: 91,
     number: 'FRD6213',
   },
   {
-    id: 93,
+    id: 92,
     number: 'GBP7600',
+  },
+  {
+    id: 93,
+    number: 'DLS9381',
+  },
+  {
+    id: 94,
+    number: 'EMW4985',
+  },
+  {
+    id: 95,
+    number: 'COH2444',
+  },
+  {
+    id: 96,
+    number: 'CQN5H89',
+  },
+  {
+    id: 97,
+    number: 'PAH9I13',
+  },
+  {
+    id: 98,
+    number: 'EJA1C63',
+  },
+  {
+    id: 99,
+    number: 'RNJ5H35',
+  },
+  {
+    id: 100,
+    number: 'OAI8D15',
   },
 ];
